ba889b066f261aedfe5327483be394651ffc4e1b
matthias <matthias@calvin>
2016-03-29 14:24:33 +0200

    - removed jade - simplified hello world

--- src/start.ts
+++ src/start.ts
@@ -1,25 +1,12 @@
 import config from "./config";
 
 import * as express from "express";
-
-const HTTP_OK = 200;
-
 let app = express();
 
-app.set("view engine", "jade");
-
 app.get("/", (req, res) => {
-	res.status(HTTP_OK);
-	res.type("text/html");
-
-	res.render("index", {
-		title: "typescript-node-express",
-		message: "Hello World!"
-	});
-
-	res.end();
+	res.send("Hello World!");
 });
 
 app.listen(config.port, () => {
-	console.log(`Listening on port ${config.port}...`)
+	console.log(`Example app listening on port ${config.port}!`)
 });
