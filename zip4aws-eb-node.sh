51cb243cbc1cbdec20c43c982031245e5ef8aceb
matthias.path <matthias.path@mucqmalt021>
2016-03-29 15:49:16 +0200

    fix: project name from package.json

--- zip4aws-eb-node.sh
+++ zip4aws-eb-node.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 PROJECT_ROOT="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_NAME="$(basename "$PROJECT_ROOT")"
-
-VERSION="$(cat "$PROJECT_ROOT/src/package.json" | grep '"version"' | cut -d\" -f4)"
+PROJECT_NAME="$(cat "$PROJECT_ROOT/src/package.json" | grep '"name"' | cut -d\" -f4)"
+PROJECT_VERSION="$(cat "$PROJECT_ROOT/src/package.json" | grep '"version"' | cut -d\" -f4)"
 
 rsync -vrLkpogt --delete-before --progress \
 	--exclude .DS_Store \
@@ -12,7 +11,7 @@ rsync -vrLkpogt --delete-before --progress \
 	--exclude typings \
 		"$PROJECT_ROOT/src" "$PROJECT_ROOT/tmp"
 
-ZIP="$PROJECT_ROOT/$PROJECT_NAME-$VERSION.zip"
+ZIP="$PROJECT_ROOT/$PROJECT_NAME-$PROJECT_VERSION.zip"
 rm -f "$ZIP"
 
 cd "$PROJECT_ROOT/tmp/src"
